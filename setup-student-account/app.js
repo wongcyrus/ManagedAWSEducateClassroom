f3161830e84da16d5a0041cb555a1562f18b7c76
EC2 Default User <ec2-user@ip-172-31-34-73.ec2.internal>
2020-04-01 06:17:41 +0000

    Add aws-ses-inbox support for Set Student Account.

--- setup-student-account/app.js
+++ setup-student-account/app.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const studentAccountTable = process.env.StudentAccountTable;
 const dynamo = new AWS.DynamoDB.DocumentClient();
-
+const s3 = new AWS.S3();
 
 const extractKeys = rawKey => {
     const accessKeyStartIndex = rawKey.indexOf("aws_access_key_id=") + "aws_access_key_id=".length;
@@ -10,8 +10,17 @@ const extractKeys = rawKey => {
     const secretKeyStartIndex = rawKey.indexOf("aws_secret_access_key=") + "aws_secret_access_key=".length;
     const secretAccessKey = rawKey.substring(secretKeyStartIndex, rawKey.indexOf("aws_session_token=")).replace(/(\r\n|\n|\r)/gm, "");
     const secretSessionTokenIndex = rawKey.indexOf("aws_session_token=") + "aws_session_token=".length;
-    const sessionToken = rawKey.substring(secretSessionTokenIndex).replace(/(\r\n|\n|\r)/gm, "");
+    const sessionToken = rawKey.substring(secretSessionTokenIndex, secretSessionTokenIndex + 368).replace(/(\r\n|\n|\r)/gm, "");
     return { accessKeyId, secretAccessKey, sessionToken };
+};
+
+const getS3File = async(bucket, key) => {
+    const params = {
+        Bucket: bucket,
+        Key: key
+    };
+    const response = await s3.getObject(params).promise();
+    return response.Body.toString();
 };
 
 const initStudentAccount = async(email, rawKey) => {
@@ -65,6 +74,16 @@ const initStudentAccount = async(email, rawKey) => {
 };
 
 exports.lambdaHandler = async(event, context) => {
+    console.log(event);
+    if (event.Records) {
+        let message = JSON.parse((JSON.parse(event.Records[0].body)).Message);
+        console.log(message);
+        let { inboxBucket, trimedEmailJson } = message;
+        const trimedEmailJsonContent = await getS3File(inboxBucket,trimedEmailJson);
+        const emailBody = JSON.parse(trimedEmailJsonContent).content;
+        console.log(emailBody);
+        await initStudentAccount(message.sender, emailBody);
+    }
     await initStudentAccount(event.email, event.key);
     return "OK";
 };
